9b58e14694b69d3c9d7ba8dfd8975865711253a9
karank85 <46368711+karank85@users.noreply.github.com>
2020-11-27 20:13:03 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import discord 
+from discord.ext import commands
+
+from sympy import *
+
+
+client = commands.Bot(command_prefix="%")
+
+@client.event
+async def on_ready():
+    print("Math bot is ready!")
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+@client.command()
+async def sine(ctx, *, question):
+    x = symbols('x')
+    answer = sin(question)
+    await ctx.send(f'Question: {question}\nAnswer: {answer}')
+
+@client.command()
+async def tangent(ctx, *, question):
+    x = symbols('x')
+    answer = tan(question)
+    await ctx.send(f'Question: {question}\nAnswer: {answer}')
+
+@client.command()
+async def cosine(ctx, *, question):
+    x = symbols('x')
+    answer = cos(question)
+    await ctx.send(f'Question: {question}\nAnswer: {answer}')
+
+@client.command()
+async def int(ctx, *, question):
+    x = symbols('x')
+    answer = integrate(question)
+    await ctx.send(f'Question: {"Integrate " + question + " dx"}\nAnswer: {answer}')
+
+
+
+
+
+client.run('ITS A SECRET :D')
+
+
